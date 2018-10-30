c9b4786437f959cacd98a1b996363bdb7b806940
tomaszpach <tomaszpach@icloud.com>
2018-10-30 10:45:41 +0100

    example redux, dispatch actions and subscribe

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
+
+const initialState = {
+    todos: [],
+    posts: []
+};
+
+function myReducer(state = initialState, action) {
+    if (action.type === 'ADD_TODO') {
+        return {
+            ...state,
+            todos: [...state.todos, action.todo]
+        }
+    }
+
+    if (action.type === 'ADD_POST') {
+        return {
+            ...state,
+            posts: [...state.posts, action.post]
+        }
+    }
+}
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {
+   console.log(store.getState())
+});
+
+const todoAction = { type: 'ADD_TODO', todo: 'buy milk' };
+const todoAction2 = { type: 'ADD_TODO', todo: 'buy milk 2' };
+const postAction = { type: 'ADD_POST', post: 'egg hunt with yoshi' };
+
+store.dispatch(todoAction);
+store.dispatch(todoAction2);
+store.dispatch(postAction);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
