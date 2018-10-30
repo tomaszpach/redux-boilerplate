4af00db7db7e2c72816b9092526542567a1ce023
tomaszpach <tomaszpach@icloud.com>
2018-10-30 08:36:26 +0100

    rendering using conditional (ternary) operator

--- src/Children.js
+++ src/Children.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 // UI component without state made by function
 const Children = ({characters}) => {
+    // const charactersList = characters.map(character => {
+    //     return (
+    //         <div className="character" key={character.id}>
+    //             <div>Name: {character.name}</div>
+    //             <div>Age: {character.age}</div>
+    //             <div>Belt: {character.belt}</div>
+    //         </div>
+    //     )
+    // });
+
     const charactersList = characters.map(character => {
-        return (
+        return character.age > 20 ? (
             <div className="character" key={character.id}>
                 <div>Name: {character.name}</div>
                 <div>Age: {character.age}</div>
                 <div>Belt: {character.belt}</div>
             </div>
-        )
+        ) : null;
     });
 
     return (
